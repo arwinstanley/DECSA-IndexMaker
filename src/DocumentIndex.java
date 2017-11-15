940ba24835a8a52c7b3d346d0bb6c40ceb1f9ba6
arwinstanley <winstanleya@bishopireton.org>
2017-11-15 12:47:12 -0500

    Was guud Folks

--- src/DocumentIndex.java
+++ src/DocumentIndex.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class DocumentIndex {
-
+private TreeMap<String, IndexEntry> blueTree;
 }
--- src/IndexEntry.java
+++ src/IndexEntry.java
@@ -1,4 +1,18 @@
-
-public class IndexEntry {
-
+import java.util.*;
+public class IndexEntry implements Comparable<IndexEntry>   {
+	private TreeSet<Integer> lines;
+	private String birdUp;
+public IndexEntry(String word) {
+	birdUp = word;
+	lines = new TreeSet<Integer>();
+}
+public void add(int num) {
+	
+}
+public String getWord() {
+	return birdUp;
+}
+public int compareTo(IndexEntry other) {
+	
+}
 }
