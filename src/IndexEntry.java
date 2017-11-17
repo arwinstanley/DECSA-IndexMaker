69cd0e6430fa54761a8330d40ae513bd071eaa28
Alex Winstanley <arwds2@gmail.com>
2017-11-17 11:52:05 -0500

    Update IndexEntry.java

--- src/IndexEntry.java
+++ src/IndexEntry.java
@@ -8,21 +8,51 @@ import java.util.*;
 public class IndexEntry implements Comparable<IndexEntry>   {
 	private TreeSet<Integer> lines;
 	private String gloWord; //It was 'birdUp' but I was told this is unproffesional
+        /**
+	 * 
+	 * One parameter constructor that takes in a String word, which is then indexed as many times as it exists
+	 * 
+	 * @param word is the word that your indexing
+	 */
 public IndexEntry(String word) {
 	gloWord = word;
 	lines = new TreeSet<Integer>();
 }
+	/**
+	 * 
+	 * Adds a number to the list of line numbers
+	 * 
+	 * @param num is the line number of another index of this word
+	 */
 public void add(int num) {
 	if (!(lines.contains(num)))
 				lines.add(num);
 }
+        /**
+	 * 
+	 * Returns the indexing 
+	 * 
+	 * @param none
+	 */
 public String getWord() {
 	return gloWord;
 }
+        /**
+	 * 
+	 * returns an int that shows the differences between two IndexEntrys
+	 * 
+	 * @param other is a seconfd instance of IndexEntry to compare to another
+	 */
 	@Override
 public int compareTo(IndexEntry other) {
 	return this.gloWord.compareTo(other.getWord());
 }
+	        /**
+	 * 
+	 * returns an String that shows what the object is
+	 * 
+	 * @param none
+	 */
 public String toString() {
 	String output = gloWord + " ";	
 	for (Integer entry : lines) {
